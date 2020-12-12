9ce400d3826b30b95104e1fb4d51719efbd9c5ae
Alex K <lexa69911@gmail,com>
2020-12-13 00:02:55 +0400

    create base interface for database-services

---
+++ src/main/java/com/example/demo/service/base/BaseService.java
@@ -0,0 +1,16 @@
+package com.example.demo.service.base;
+
+import com.example.demo.dao.domain.base.BaseDto;
+import com.example.demo.dao.domain.base.BaseEntity;
+
+import java.util.List;
+
+public interface BaseService<E extends BaseEntity, D extends BaseDto> {
+
+    E create(D dto);
+    E save(E entity);
+    E delete(E entity);
+    E findById(Long id);
+    List<E> findAll();
+
+}
