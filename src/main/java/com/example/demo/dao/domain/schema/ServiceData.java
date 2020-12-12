58b09dde83cf0da9c1516ace26aa0e32f36fcf70
Alex K <lexa69911@gmail,com>
2020-12-12 22:05:53 +0400

    add *Data classes

---
+++ src/main/java/com/example/demo/dao/domain/schema/ServiceData.java
@@ -0,0 +1,11 @@
+package com.example.demo.dao.domain.schema;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ServiceData {
+}
