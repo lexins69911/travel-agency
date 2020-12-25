b1ff49dda140f980bb01c86293be051310d5c3e3
Alex K <lexa69911@gmail,com>
2020-12-25 23:10:53 +0400

    add new methods for mapping Lists

--- src/main/java/com/example/demo/service/mapper/HotelMapper.java
+++ src/main/java/com/example/demo/service/mapper/HotelMapper.java
@@ -4,10 +4,14 @@ import com.example.demo.dao.domain.dto.HotelDto;
 import com.example.demo.dao.domain.model.HotelEntity;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface HotelMapper {
 
     HotelEntity toEntity(HotelDto dto);
     HotelDto toDto(HotelEntity entity);
 
+    List<HotelEntity> toListEntity(List<HotelDto> list);
+    List<HotelDto> toListDto(List<HotelEntity> list);
 }
--- src/main/java/com/example/demo/service/mapper/TourMapper.java
+++ src/main/java/com/example/demo/service/mapper/TourMapper.java
@@ -4,10 +4,14 @@ import com.example.demo.dao.domain.dto.TourDto;
 import com.example.demo.dao.domain.model.TourEntity;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface TourMapper {
 
     TourEntity toEntity(TourDto dto);
     TourDto toDto(TourEntity entity);
 
+    List<TourDto> toListDto(List<TourEntity> list);
+    List<TourEntity> toListEntity(List<TourDto> list);
 }
