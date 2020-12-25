eac1232c191595d68550ea1e5fa375db10bda62d
Alex K <lexa69911@gmail,com>
2020-12-26 03:59:49 +0400

    add support for scheduling

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(securedEnabled = true)
+@EnableScheduling
 public class DemoApplication {
 
     public static void main(String[] args) {
