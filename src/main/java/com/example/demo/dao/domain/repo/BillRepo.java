ba0a3ce38b0af6a17bfbe480efb879cf665e3c4c
Alex K <lexa69911@gmail,com>
2020-12-26 03:58:36 +0400

    add new method

--- src/main/java/com/example/demo/dao/domain/repo/BillRepo.java
+++ src/main/java/com/example/demo/dao/domain/repo/BillRepo.java
@@ -1,6 +1,7 @@
 package com.example.demo.dao.domain.repo;
 
 import com.example.demo.dao.domain.model.BillEntity;
+import com.example.demo.dao.domain.model.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -8,5 +9,6 @@ import java.util.List;
 public interface BillRepo extends CrudRepository<BillEntity, Long> {
 
     List<BillEntity> findByTourId (Long id);
+    List<BillEntity> findAllByUser (UserEntity userEntity);
 
 }
