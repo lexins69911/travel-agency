f59ee552949aed7d4dfb740c8d27411f3fd387c1
Alex K <lexa69911@gmail,com>
2020-12-26 03:59:29 +0400

    fix imports

--- src/main/java/com/example/demo/dao/domain/model/UserEntity.java
+++ src/main/java/com/example/demo/dao/domain/model/UserEntity.java
@@ -1,15 +1,12 @@
 package com.example.demo.dao.domain.model;
 
 import com.example.demo.dao.domain.base.BaseEntity;
-import com.example.demo.dao.domain.schema.Role;
+import com.example.demo.dao.domain.schema.enums.Role;
 import com.example.demo.dao.domain.schema.jsonb.UserData;
-import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.Type;
-import org.hibernate.annotations.TypeDef;
-import org.hibernate.annotations.TypeDefs;
 
 import javax.persistence.*;
 import java.util.Set;
