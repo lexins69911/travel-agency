95bd4df66f4929f45468079fbfb2a4c92faec70f
Alex K <lexa69911@gmail,com>
2020-12-26 03:59:20 +0400

    fix imports

--- src/main/java/com/example/demo/dao/domain/model/ServiceEntity.java
+++ src/main/java/com/example/demo/dao/domain/model/ServiceEntity.java
@@ -1,18 +1,13 @@
 package com.example.demo.dao.domain.model;
 
 import com.example.demo.dao.domain.base.BaseEntity;
-import com.example.demo.dao.domain.schema.ServiceCategory;
 import com.example.demo.dao.domain.schema.jsonb.ServiceData;
-import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.Type;
-import org.hibernate.annotations.TypeDef;
-import org.hibernate.annotations.TypeDefs;
 
 import javax.persistence.*;
-import java.math.BigInteger;
 
 @Entity
 @Table(name = "service")
