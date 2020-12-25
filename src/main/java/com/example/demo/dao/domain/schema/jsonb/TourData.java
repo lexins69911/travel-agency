b53a0619473474e9b2f8aea06287627f882dfa77
Alex K <lexa69911@gmail,com>
2020-12-26 01:28:46 +0400

    add fields in jsonb

--- src/main/java/com/example/demo/dao/domain/schema/jsonb/TourData.java
+++ src/main/java/com/example/demo/dao/domain/schema/jsonb/TourData.java
@@ -4,10 +4,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class TourData implements Serializable {
+
+    private LocalDateTime dateArrival;
+    private LocalDateTime dateDepartment;
+
 }
