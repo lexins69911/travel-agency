03969b8158ec72d501930e1a0d1e7d9fe91a1867
Alex K <lexa69911@gmail,com>
2020-12-26 03:59:58 +0400

    add list mapper

--- src/main/java/com/example/demo/service/mapper/UserMapper.java
+++ src/main/java/com/example/demo/service/mapper/UserMapper.java
@@ -5,10 +5,14 @@ import com.example.demo.dao.domain.model.UserEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
     UserDto toDto (UserEntity entity);
     UserEntity toEntity(UserDto dto);
 
+    List<UserEntity> toListEntity(List<UserDto> list);
+    List<UserDto> toListDto(List<UserEntity> list);
 }
