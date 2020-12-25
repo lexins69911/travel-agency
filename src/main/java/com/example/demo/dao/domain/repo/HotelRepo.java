68097a3f61fe4ac23158545fcfdf0e33b0504b68
Alex K <lexa69911@gmail,com>
2020-12-25 23:09:34 +0400

    add new method to repo

--- src/main/java/com/example/demo/dao/domain/repo/HotelRepo.java
+++ src/main/java/com/example/demo/dao/domain/repo/HotelRepo.java
@@ -4,8 +4,11 @@ import com.example.demo.dao.domain.model.HotelEntity;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface HotelRepo extends CrudRepository<HotelEntity, Long> {
 
     List<HotelEntity> findAllByName (String name);
+    List<HotelEntity> findAllByNameContains(String partOfName);
+    List<HotelEntity> findAllByCountryEntityName(String name);
 }
