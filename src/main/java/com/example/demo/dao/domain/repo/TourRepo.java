dd7f5cf634774f0fff0c8da02d004f6e3ae3e085
Alex K <lexa69911@gmail,com>
2020-12-27 02:40:39 +0400

    fix method for search tour by single date

--- src/main/java/com/example/demo/dao/domain/repo/TourRepo.java
+++ src/main/java/com/example/demo/dao/domain/repo/TourRepo.java
@@ -9,8 +9,8 @@ import java.util.Optional;
 
 public interface TourRepo extends CrudRepository<TourEntity, Long> {
 
-    List<TourEntity> findAllByDateStart (LocalDate dateStart);
-    List<TourEntity> findAllByDateEnd (LocalDate dateEnd);
+    List<TourEntity> findAllByDateStartGreaterThanEqual (LocalDate dateStart);
+    List<TourEntity> findAllByDateEndLessThanEqual (LocalDate dateEnd);
     List<TourEntity> findAllByDateStartGreaterThanEqualAndDateEndLessThanEqual (LocalDate start, LocalDate end);
     List<TourEntity> findAllByNameContains(String partName);
     Optional<TourEntity> findByHotelId(Long hotelId);
