01f2abcb80f8a3240b802b77c10d5b719a0a6c7d
Alex K <lexa69911@gmail,com>
2020-12-26 19:07:57 +0400

    add id to base dto

--- src/main/java/com/example/demo/dao/domain/base/BaseDto.java
+++ src/main/java/com/example/demo/dao/domain/base/BaseDto.java
@@ -1,9 +1,15 @@
 package com.example.demo.dao.domain.base;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.io.Serializable;
 
+@Getter
+@Setter
 public class BaseDto implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    private Long id;
 }
